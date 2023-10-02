1d1a436dad1872596cd8730628eba84f2fbdba2f
AmatsuZero <jzh16s@hotmail.com>
2023-10-01 19:05:02 -0700

    ADD: 扩展词法单元和词法分析器 & REPL

--- token/token.go
+++ token/token.go
@@ -11,8 +11,15 @@ const (
 	INT   TokenType = "INT"   // 1343456
 
 	// 运算符
-	ASSIGN TokenType = "="
-	PLUS   TokenType = "+"
+	ASSIGN   TokenType = "="
+	PLUS     TokenType = "+"
+	MINUS    TokenType = "-"
+	BANG     TokenType = "!"
+	ASTERISK TokenType = "*"
+	SLASH    TokenType = "/"
+
+	LT TokenType = "<"
+	GT TokenType = ">"
 
 	// 分隔符
 	COMMA     TokenType = ","
@@ -26,11 +33,23 @@ const (
 	// 关键字
 	FUNCTION TokenType = "FUNCTION"
 	LET      TokenType = "LET"
+	TRUE     TokenType = "TRUE"
+	FALSE    TokenType = "FALSE"
+	IF       TokenType = "IF"
+	ELSE     TokenType = "ELSE"
+	RETURN   TokenType = "RETURN"
+	EQ       TokenType = "=="
+	NOT_EQ   TokenType = "!="
 )
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 // 通过检查关键字表来判断给定的标识符是否是关键字
